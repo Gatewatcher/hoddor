266fc67570da4f5e0816e52b433dbad8798b5604
David Loiret <loiret.d@gmail.com>
2024-12-24 09:01:35 +0100

    fix: update vault according to lib changes

--- playground/src/vault.ts
+++ playground/src/vault.ts
@@ -18,67 +18,51 @@ export class VaultWorker {
           resolve(message.data.result)
         }
       }
-      
+
       this.worker.addEventListener('message', handler)
       this.worker.postMessage({ type, payload })
     })
   }
 
-  async createVault(password: string, namespace: string, data: any): Promise<void> {
-    await this.send('create_vault', { password, namespace, data })
+  async createVault(vaultName: string, password: string, namespace: string, data: any, expiresInSeconds?: BigInt): Promise<void> {
+    await this.send('create_vault', { vaultName, password, namespace, data, expiresInSeconds })
   }
 
-  async readFromVault(password: string, namespace: string): Promise<any> {
-    return this.send('read_from_vault', {password, namespace});
+  async readFromVault(vaultName: string, password: string, namespace: string, expiresInSeconds?: BigInt): Promise<any> {
+    return this.send('read_from_vault', { vaultName, password, namespace, expiresInSeconds });
   }
 
-  async upsertVault(password: string, namespace: string, data: any): Promise<void> {
-    await this.send('upsert_vault', { password, namespace, data })
+  async upsertVault(vaultName: string, password: string, namespace: string, data: any, expiresInSeconds?: BigInt, replaceIfExists?: boolean): Promise<void> {
+    await this.send('upsert_vault', { vaultName, password, namespace, data, expiresInSeconds, replaceIfExists })
   }
 
-  async removeFromVault(password: string, namespace: string): Promise<void> {
-    await this.send('remove_from_vault', { password, namespace })
+  async removeFromVault(vaultName: string, password: string, namespace: string): Promise<void> {
+    await this.send('remove_from_vault', { vaultName, password, namespace })
   }
 
-  async listNamespaces(password: string): Promise<string[]> {
-    return this.send('list_namespaces', {password})
+  async removeVault(vaultName: string, password: string): Promise<void> {
+    await this.send('remove_vault', { vaultName, password })
+  }
+
+  async listNamespaces(vaultName: string, password: string): Promise<string[]> {
+    return this.send('list_namespaces', { vaultName, password })
   }
 
   async setDebugMode(enabled: boolean): Promise<void> {
     await this.send('set_debug_mode', { enabled })
   }
 
-  async createVaultWithName(vaultName: string, password: string, namespace: string, data: any): Promise<void> {
-    await this.send('create_vault_with_name', { vaultName, password, namespace, data })
-  }
-
-  async readFromVaultWithName(vaultName: string, password: string, namespace: string): Promise<any> {
-    return this.send('read_from_vault_with_name', { vaultName, password, namespace })
-  }
-
-  async upsertVaultWithName(vaultName: string, password: string, namespace: string, data: any, expiration?: any): Promise<void> {
-    await this.send('upsert_vault_with_name', { vaultName, password, namespace, data, expiration })
-  }
-
-  async removeVaultWithName(vaultName: string, password: string, namespace: string): Promise<void> {
-    await this.send('remove_vault_with_name', { vaultName, password, namespace })
-  }
-
   async listVaults(): Promise<string[]> {
     return this.send('list_vaults', {})
   }
 
-  async listNamespacesWithName(vaultName: string, password: string): Promise<string[]> {
-    return this.send('list_namespaces_with_name', { vaultName, password })
-  }
-
-  async exportVault(password: string): Promise<Uint8Array> {
-    const response = await this.send('export_vault', { password });
+  async exportVault(vaultName: string, password: string): Promise<Uint8Array> {
+    const response = await this.send('export_vault', { vaultName, password });
     return response;
   }
 
-  async importVault(password: string, data: Uint8Array): Promise<void> {
-    await this.send('import_vault', { password, data });
+  async importVault(vaultName: string, password: string, data: Uint8Array): Promise<void> {
+    await this.send('import_vault', { vaultName, password, data });
   }
 
   async configureCleanup(intervalSeconds: number): Promise<void> {
