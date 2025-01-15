172d28d11526c09103829b7fa684493a51f2ab73
benoit.chassignol <benoit.chassignol@gatewatcher.com>
2025-01-15 13:54:09 +0100

    fix(webauthn): webauthn impl - frontend (#4)

--- hoddor/src/auth/mod.rs
+++ hoddor/src/auth/mod.rs
@@ -1,70 +1,57 @@
 use js_sys::Uint8Array;
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{
-    Crypto, PublicKeyCredential, PublicKeyCredentialRpEntity, PublicKeyCredentialUserEntity,
-};
+use web_sys::{Crypto, PublicKeyCredential};
 use webauthn::{webauthn_create, webauthn_get};
 
 use crate::console::*;
-mod webauthn;
+pub mod webauthn;
 
 pub fn crypto() -> Result<Crypto, JsValue> {
     web_sys::window().ok_or(JsValue::UNDEFINED)?.crypto()
 }
 
-pub fn gen_random<const N: usize>() -> Result<[u8; N], JsValue> {
+fn gen_random<const N: usize>() -> Result<[u8; N], JsValue> {
     let mut result: [u8; N] = [0; N];
     crypto()?.get_random_values_with_u8_array(&mut result)?;
     Ok(result)
 }
 
-static UUID_TEST: &str = "dwadawadw.awdawd.awdawd.dawdaw";
-static SALT_TEST: [u8; 32] = [
-    167, 229, 117, 95, 216, 60, 55, 245, 101, 198, 174, 106, 171, 68, 8, 211, 69, 45, 61, 22, 46,
-    121, 232, 219, 42, 246, 223, 109, 78, 30, 226, 56,
-];
-
 #[wasm_bindgen]
-pub async fn create_credential(username: &str) -> Result<PublicKeyCredential, JsValue> {
+pub async fn create_credential(
+    name: &str,
+    rp_id: &str,
+    prf_salt: &Uint8Array,
+    cred_id: Option<Uint8Array>,
+) -> Result<PublicKeyCredential, JsValue> {
     log(&format!("Init credential creation"));
 
-    // let prf_salt: [u8; 32] = gen_random()?;
-    let prf_salt: [u8; 32] = SALT_TEST;
-    let challenge: [u8; 32] = gen_random()?;
+    let challenge = Uint8Array::from(gen_random::<32>()?.as_slice());
+
+    let cred_id = match cred_id {
+        None => Uint8Array::from(gen_random::<32>()?.as_slice()),
+        Some(cred_id) => cred_id.clone(),
+    };
 
     Ok(JsFuture::from(webauthn_create(
-        challenge.as_slice(),
-        PublicKeyCredentialUserEntity::new(
-            username,
-            username,
-            &Uint8Array::from(UUID_TEST.as_bytes().to_vec().as_slice()),
-        ),
-        {
-            let pk_rp_entity = PublicKeyCredentialRpEntity::new("Vault");
-            pk_rp_entity.set_id("localhost");
-            pk_rp_entity
-        },
-        prf_salt,
+        &challenge, name, rp_id, prf_salt, &cred_id,
     )?)
     .await?
     .into())
 }
 
 #[wasm_bindgen]
-pub async fn get_credential() -> Result<PublicKeyCredential, JsValue> {
+pub async fn get_credential(
+    prf_salt: &Uint8Array,
+    cred_id: Option<Uint8Array>,
+) -> Result<PublicKeyCredential, JsValue> {
     log(&format!("Init credential get"));
 
-    // let prf_salt: [u8; 32] = gen_random()?;
-    let prf_salt: [u8; 32] = SALT_TEST;
-    let challenge: [u8; 32] = gen_random()?;
+    let challenge = Uint8Array::from(gen_random::<32>()?.as_slice());
 
-    Ok(JsFuture::from(webauthn_get(
-        challenge.as_slice(),
-        UUID_TEST,
-        "localhost",
-        prf_salt,
-    )?)
-    .await?
-    .into())
+    Ok(
+        JsFuture::from(webauthn_get(&challenge, "localhost", prf_salt, cred_id)?)
+            .await?
+            .into(),
+    )
 }
--- hoddor/src/auth/webauthn.rs
+++ hoddor/src/auth/webauthn.rs
@@ -1,8 +1,8 @@
-use js_sys::{Array, ArrayBuffer, Object, Promise, Uint8Array};
+use js_sys::{Array, ArrayBuffer, Object, Promise, Reflect, Uint8Array};
 use wasm_bindgen::{JsCast, JsError, JsValue};
 use web_sys::{
     AuthenticationExtensionsClientInputs, CredentialCreationOptions, CredentialRequestOptions,
-    PublicKeyCredentialCreationOptions, PublicKeyCredentialDescriptor,
+    PublicKeyCredential, PublicKeyCredentialCreationOptions, PublicKeyCredentialDescriptor,
     PublicKeyCredentialParameters, PublicKeyCredentialRequestOptions, PublicKeyCredentialRpEntity,
     PublicKeyCredentialType, PublicKeyCredentialUserEntity, UserVerificationRequirement, Window,
 };
@@ -12,26 +12,32 @@ use crate::console::*;
 static SECURE_ALGORITHM: &[i32; 3] = &[-7, -257, -8];
 
 pub fn webauthn_create(
-    challenge: &[u8],
-    user: PublicKeyCredentialUserEntity,
-    rp_entity: PublicKeyCredentialRpEntity,
-    prf_salt: [u8; 32],
+    challenge: &Uint8Array,
+    name: &str,
+    rp_id: &str,
+    prf_salt: &Uint8Array,
+    cred_id: &Uint8Array,
 ) -> Result<Promise, JsError> {
     log(&format!("Create webauthn"));
 
+    let pk_rp_entity = PublicKeyCredentialRpEntity::new(name);
+    pk_rp_entity.set_id(rp_id);
+
+    let pk_user = PublicKeyCredentialUserEntity::new(name, name, cred_id);
+
     let pk_options = PublicKeyCredentialCreationOptions::new(
-        &Uint8Array::from(challenge),
-        &SECURE_ALGORITHM.iter().map(|alg| PublicKeyCredentialParameters::new(
-            alg.clone(),
-            PublicKeyCredentialType::PublicKey,
-        )).collect::<Array>(),
-        &rp_entity,
-        &user,
+        challenge,
+        &SECURE_ALGORITHM
+            .iter()
+            .map(|alg| {
+                PublicKeyCredentialParameters::new(alg.clone(), PublicKeyCredentialType::PublicKey)
+            })
+            .collect::<Array>(),
+        &pk_rp_entity,
+        &pk_user,
     );
 
-    pk_options.set_extensions(&prf_extension_eval(
-        &Uint8Array::from(prf_salt.as_slice()).buffer(),
-    ));
+    pk_options.set_extensions(&prf_extension_eval(&prf_salt.buffer()));
 
     let cred_options = CredentialCreationOptions::new();
     cred_options.set_public_key(&pk_options);
@@ -44,28 +50,31 @@ pub fn webauthn_create(
 }
 
 pub fn webauthn_get(
-    challenge: &[u8],
-    user_uuid: &str,
+    challenge: &Uint8Array,
     rp_id: &str,
-    prf_salt: [u8; 32],
+    prf_salt: &Uint8Array,
+    cred_id: Option<Uint8Array>,
 ) -> Result<Promise, JsError> {
     log(&format!("Get webauthn"));
 
-    let pk_options = PublicKeyCredentialRequestOptions::new(&Uint8Array::from(challenge));
-    pk_options.set_allow_credentials(
-        &Some(PublicKeyCredentialDescriptor::new(
-            &Uint8Array::from(user_uuid.as_bytes().to_vec().as_slice()),
-            PublicKeyCredentialType::PublicKey,
-        ))
-        .into_iter()
-        .collect::<Array>(),
-    );
+    let pk_options = PublicKeyCredentialRequestOptions::new(&challenge);
+
+    match cred_id {
+        None => (),
+        Some(cred_id) => {
+            let allow_creds = Array::new();
+            allow_creds.push(&PublicKeyCredentialDescriptor::new(
+                &Uint8Array::from(cred_id),
+                PublicKeyCredentialType::PublicKey,
+            ));
+            pk_options.set_allow_credentials(&allow_creds);
+        }
+    };
+
     pk_options.set_rp_id(&rp_id);
     pk_options.set_user_verification(UserVerificationRequirement::Required);
 
-    pk_options.set_extensions(&prf_extension_eval(
-        &Uint8Array::from(prf_salt.as_slice()).buffer(),
-    ));
+    pk_options.set_extensions(&prf_extension_eval(&prf_salt.buffer()));
 
     let cred_options = CredentialRequestOptions::new();
     cred_options.set_public_key(&pk_options);
@@ -91,6 +100,15 @@ pub fn prf_extension_eval(salt: &ArrayBuffer) -> AuthenticationExtensionsClientI
         .dyn_into::<JsValue>()
         .unwrap(),
     )
+}
+
+pub fn prf_first_output(cred: &PublicKeyCredential) -> Result<Uint8Array, JsValue> {
+    let extensions: Object = cred.get_client_extension_results().dyn_into()?;
+    let prf_output: Result<Uint8Array, JsValue> = Reflect::get(&extensions, &"prf".into())
+        .and_then(|prf| Reflect::get(&prf, &"results".into()))
+        .and_then(|prf_results| Reflect::get(&prf_results, &"first".into()))
+        .map(|first| Uint8Array::new(&first));
+    prf_output
 }
 
 pub fn window() -> Window {
